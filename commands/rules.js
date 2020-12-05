e20153503e3dcd6eb6efac9a8d4032ff5fca0594
OishikGuha <57310936+OishikGuha@users.noreply.github.com>
2020-12-05 16:06:31 +0530

    added fixed rules

---
+++ commands/rules.js
@@ -0,0 +1,49 @@
+module.exports = {
+    name: 'rules',
+    description: 'rulez',
+    execute(message, args) 
+    {
+        message.channel.send(
+            `
+            Welcome to this server.
+            Here are some rules that you should follow if you want to be in this server.
+
+            1. Don\'t be racist. 
+            Please do NOT be racist towards any caste, person or religion\.
+
+            2. No bullying -
+            know it\'s not intended many times but this is something that I will not tolerate. This is a real offence, and no one should do that unless they want to be in trouble.
+
+            3. No unnecessary pings. 
+            Look, I know sometimes you do it for fun, or when you are bored or something but unless the shit is serious, pinging will not result in a good way. 
+
+            4. You can say profanities in here, just not too much you edgelords. 
+
+            5. Have fun! Let\'s create a sweet ducking community and have fun. 
+
+            6. Also, please don\'t fucking spam. It\'s annoying and rather idiotic. 
+
+            If you want to talk to me then call or text me.
+
+            7.Discord TOS and Common sense applies, so please follow them too. 
+
+            8. No NSFW stuff, if you dare send that stuff in here, you will get instant ban hammer. Infact, I won\'t even read your ban appeal.
+
+            9. No pornography of any kind - Go to Church you horny devils.
+
+            10. Pinging and Mentions-I get that it\'s un-intentional sometimes, but pinging without a valid reason will not be tolerated.
+
+            11. No, I will not tolerate it if you are being mean to any staff member or me. 
+
+            12. If you know me irl, please respect my privacy!
+
+            13. I have final say in everything, I get to choose and manage stuff.
+
+            14. If any staff is abusing their powers, DM Me or Anatidaeophobia.
+
+            15. No tragedic references, jokes and comments.
+
+            16. Please respect me and my wishes. I get that it\'s un-intentional sometimes, but hurting me in any way will result in a permanent ban.`
+        );
+    }
+}
