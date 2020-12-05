873997b987a241ddca2c0e0839a0b1d3dde5e475
OishikGuha <57310936+OishikGuha@users.noreply.github.com>
2020-12-05 16:02:28 +0530

    added rules command

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ const prefix = '!!';
 const fs = require('fs');
 
 const YouTube = require("discord-youtube-api");
- 
+
 const youtube = new YouTube("google api key");
- 
+
 async function testAll() {
     const video1 = await youtube.getVideo("https://www.youtube.com/watch?v=5NPBIwQyPWE");
     const video2 = await youtube.getVideoByID("5NPBIwQyPWE");
     const video3 = await youtube.searchVideos("big poppa biggie smalls");
     const videoArray1 = await youtube.getPlaylist("https://www.youtube.com/playlist?list=PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
     const videoArray2 = await youtube.getPlaylistByID("PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
- 
+
     console.log(video1, video2, video3, videoArray1, videoArray2);
 }
 
@@ -48,31 +48,38 @@ client.on('message', message => {
     else if (command === 'ping') {
         client.commands.get('ping').execute(message, args);
     }
-    else if (command === 'joincult') 
+    else if (command === 'joincult')
     {
-        client.commands.get('joincult').execute(message, args); 
+        client.commands.get('joincult').execute(message, args);
     }
-    else if (command === 'ban') 
+    else if (command === 'ban')
     {
-        client.commands.get('ban').execute(message, args); 
+        client.commands.get('ban').execute(message, args);
     }
-    else if (command === 'trueban') 
+    else if (command === 'trueban')
     {
         client.commands.get('trueban').execute(message, args);
     }
-    else if (command === 'youtube') 
+    else if (command === 'youtube')
     {
         client.commands.get('youtube').execute(message, args);
     }
-    else if (command === 'party') 
+    else if (command === 'party')
     {
         client.commands.get('party').execute(message, args);
     }
-    else if (command === 'praise') 
+    else if (command === 'praise')
     {
-        client.commands.get('praise').execute(message, args);     
+        client.commands.get('praise').execute(message, args);
+    }
+    else if (command === 'youtubesearch')
+    {
+      client.commands.get('youtubesearch').execute(message, args);
+    }
+    else if (command === 'rules')
+    {
+        client.commands.get('rules').execute(message, args);
     }
 })
 
 client.login(process.env.TOKEN);
-
